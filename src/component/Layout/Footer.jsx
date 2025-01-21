88cf951c3560514e6313dc81380a83812087c2c4
rishi0810 <rishi.1003raj@gmail.com>
2025-01-21 13:44:35 +0530

    Footer issue resolve

--- src/component/Layout/Footer.jsx
+++ src/component/Layout/Footer.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 function Footer() {
   return (
-    <footer className="relative bottom-0 bg-zinc-900 text-white border-t border-slate-800 py-4 px-24 w-full">
-      <div className="flex px-5 text-center">
-      <span class="material-symbols-outlined">
+    <footer className="relative bottom-0 bg-zinc-900 text-white border-t border-slate-800 py-4 px-15 w-full">
+      <div className="flex px-5 justify-center">
+      <span className="material-symbols-outlined mr-1">
             copyright
           </span>
         <p className='text-slate-300 text-lg font-poppins font-bold'>
-        
-          {new Date().getFullYear()} JeeNeeTards. All rights reserved.</p>
+          {new Date().getFullYear()} JeeNeeTards</p>
       </div>
     </footer>
   )
