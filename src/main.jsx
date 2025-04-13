10ef430cb9a69754718aee6756f5f121438b95a2
Gaurav Sharma <gaurav.sharma4@timesinternet.in>
2025-04-13 23:05:10 +0530

    sitemap for sero

--- src/main.jsx
+++ src/main.jsx
@@ -7,42 +7,19 @@ import Home from './component/Pages/Home.jsx'
 import Neet from './component/Pages/Neet.jsx'
 import Jee from "./component/Pages/Jee.jsx"
 import Courses from './component/Pages/Courses.jsx'
+import Sitemap from './component/Pages/Sitemap.jsx'
 
 const router = createBrowserRouter(
-
   createRoutesFromElements(
-    <Route
-    
-    path = ''
-    element = {<App/>}
-    >
-      <Route
-      path = ''
-      element = {<Home/>}
-      />
-      <Route
-      path = 'neet'
-      element = {<Neet/>}
-      />
-      <Route
-      path = "jee"
-      element = {<Jee/>}
-      />
-      <Route
-      path = "courses"
-      element = {<Courses/>}
-      />
-
-
-        
+    <Route path = '' element = {<App/>}>
+      <Route path = '' element = {<Home/>} />
+      <Route path = 'neet' element = {<Neet/>} />
+      <Route path = "jee" element = {<Jee/>} />
+      <Route path = "courses" element = {<Courses/>} />
+      <Route path = "sitemap.xml" element = {<Sitemap/>} />
     </Route>
-
-
   )
-
 )
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
