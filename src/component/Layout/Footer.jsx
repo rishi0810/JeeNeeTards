f9194dc8f98798e2abdc8f64dbe955f79740e650
Gaurav Sharma <gaurav.sharma4@timesinternet.in>
2025-04-20 23:20:49 +0530

    footer updated

--- src/component/Layout/Footer.jsx
+++ src/component/Layout/Footer.jsx
@@ -6,8 +6,9 @@ function Footer() {
       <span className="material-symbols-outlined mr-1">
             copyright
           </span>
-        <p className='text-slate-300 text-lg font-poppins font-bold'>
-          {new Date().getFullYear()} JeeNeeTards</p>
+        <p className='text-slate-300 text-sm md:text-lg font-poppins font-bold whitespace-nowrap'>
+          {new Date().getFullYear()} JeeNeeTards- <a href="https://www.linkedin.com/in/rishiraj2003/" className='text-blue-500'>Rishi Raj</a>
+        </p>
       </div>
     </footer>
   )
