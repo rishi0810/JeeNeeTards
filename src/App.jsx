98471c7d9e75d304ed1a2fc4a82a13634b396dad
Gaurav Sharma <gaurav.sharma4@timesinternet.in>
2025-04-27 22:36:05 +0530

    auto scroll fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import Footer from './component/Layout/Footer'
 import { Outlet } from 'react-router-dom'
 import { AiProvider } from './context/AiContext'
 import Ai from './component/Pages/Ai'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
 
 
   return (
   <AiProvider>
+    <ScrollToTop />
     <div style={{ userSelect: 'none' }}>
       <Header />
       <Outlet />
